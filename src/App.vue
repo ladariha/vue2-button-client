f3c89ea10e92ef1783c40dd5526e7b73257dc724
Vladimir Riha <vladimir.riha@memsource.com>
2021-10-08 15:31:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <JsonEditorVue :modelValue.sync="obj" v-bind="obj" />
+    <MyButton/>
+    <p>Mouse: {{ x }}x{{ y }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { MyButton } from "ladariha-button";
+import JsonEditorVue from "json-editor-vue";
+import { useMouse } from "@vue-demi/use-mouse";
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    JsonEditorVue, MyButton,
+  },
+  setup() {
+    const {x, y} = useMouse();
+    const obj = { a : 1}
+    return {x, y, obj};
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
